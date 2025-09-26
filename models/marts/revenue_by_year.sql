3c53bf6eb8945439c51d8d73a81917dc3e20d8a5
youssef fathi <yfathi2008@gmail.com>
2025-09-26 07:18:41 +0300

    -added new mart

---
+++ models/marts/revenue_by_year.sql
@@ -0,0 +1,31 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+with orders as (
+    select * from {{ source('tpch', 'orders') }}
+),
+
+lineitems as (
+    select * from {{ source('tpch', 'lineitem') }}
+),
+
+yearly_revenue as (
+    select 
+        extract(year from o.o_orderdate) as order_year,
+        count(distinct o.o_orderkey) as total_orders,
+        count(distinct o.o_custkey) as total_customers,
+        sum(o.o_totalprice) as total_order_value,
+        sum(l.l_extendedprice * (1 - l.l_discount)) as total_revenue,
+        avg(o.o_totalprice) as avg_order_value,
+        sum(l.l_quantity) as total_quantity_sold
+    from orders o
+    left join lineitems l
+        on o.o_orderkey = l.l_orderkey
+    group by extract(year from o.o_orderdate)
+)
+
+select * from yearly_revenue
+order by order_year
